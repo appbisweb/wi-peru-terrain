b0dcd81ba55f07f32765b02297ef23d9d45266fc
Jan Luther <45783501+appbisweb@users.noreply.github.com>
2024-10-04 07:38:14 +0000

    Animation hinzugefügt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Suspense, useRef, useMemo, useEffect } from "react";
-import { Canvas, useFrame, useUpdate, useLoader } from "react-three-fiber";
-import { Sky, OrbitControls, StandardEffects } from "drei";
+import React, { Suspense, useRef, useMemo } from "react";
+import { Canvas, useFrame, useLoader } from "react-three-fiber";
+import { OrbitControls } from "drei";
 import * as THREE from "three";
 import simplex from "simplex-noise";
 
@@ -10,15 +10,6 @@ import rockTexturePath from "./resources/rock.jpg";
 const noise = new simplex(80);
 const width = 100;
 
-function height(x, y) {
-  return (
-    noise.noise2D(x, y) / 5 +
-    noise.noise2D(x / 10, y / 10) * 0.8 +
-    noise.noise2D(x / 20, y / 20) * 0.2 +
-    noise.noise2D(x / 40, y / 40) * 8
-  );
-}
-
 const vertexShader = `
   varying vec2 vUv;
   varying vec3 vNormal;
@@ -26,7 +17,7 @@ const vertexShader = `
 
   void main() {
     vUv = uv;
-    vNormal = normal;
+    vNormal = normalize(normalMatrix * normal);
     vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
     gl_Position = projectionMatrix * mvPosition;
     #include <fog_vertex> 
@@ -46,16 +37,19 @@ const fragmentShader = `
   varying vec2 vUv;
   varying vec3 vNormal;
 
-
   void main(void) {
     vec2 uv = mod(vUv * 4.0, 1.0);
     float amount = 1.0 - min(1.0, pow((1.0 - abs(vNormal.y)) * 3.0, 4.0));
     vec4 Ca = texture2D(tOne, uv);
     vec4 Cb = texture2D(tSec, uv);
-    vec3 c = Ca.rgb * amount + Cb.rgb * (1.0 - amount); 
-    // float illum = dot(vNormal, vec3(0.0, 1.0, 0.0));
-    float illum = 1.0;
-    gl_FragColor = vec4(c * illum, 1.0);
+    vec3 c = mix(Ca.rgb, Cb.rgb, amount);
+
+    // Beleuchtungsberechnung
+    vec3 lightDir = normalize(vec3(0.5, 1.0, 0.5)); // Richtung der Lichtquelle
+    float diff = max(dot(normalize(vNormal), lightDir), 0.0);
+    vec3 diffuse = c * diff;
+
+    gl_FragColor = vec4(diffuse, 1.0);
     #include <fog_fragment>
   }
 `;
@@ -66,6 +60,11 @@ function Ground({ patchResol = 256, offsetX = 0, offsetY = 0 }) {
     rockTexturePath,
   ]);
 
+  const coeff1 = useRef(1.0);
+  const coeff2 = useRef(2.0);
+  const coeff3 = useRef(8.0);
+
+  // Erstellen der Geometrie nur einmal
   const groundGeo = useMemo(() => {
     const geo = new THREE.PlaneBufferGeometry(
       width,
@@ -74,18 +73,41 @@ function Ground({ patchResol = 256, offsetX = 0, offsetY = 0 }) {
       patchResol
     );
     geo.lookAt(new THREE.Vector3(0, 1, 0));
-    for (let i = 0; i < geo.attributes.position.count; i++) {
-      const vx = geo.attributes.position.array[i * 3];
-      const vz = geo.attributes.position.array[i * 3 + 2];
-      geo.attributes.position.array[i * 3 + 1] = height(
+    return geo;
+  }, [patchResol]);
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    const t = (elapsedTime % 6) / 6;
+
+    coeff1.current = 1.0 - t * (1.0 - 0.1);
+    coeff2.current = 2.0 - t * (2.0 - 0.1);
+    coeff3.current = 8.0 - t * (8.0 - 0.1);
+
+    const positions = groundGeo.attributes.position.array;
+    for (let i = 0; i < positions.length / 3; i++) {
+      const vx = positions[i * 3];
+      const vz = positions[i * 3 + 2];
+      positions[i * 3 + 1] = height(
         vx + offsetX,
-        vz + offsetY
+        vz + offsetY,
+        coeff1.current,
+        coeff2.current,
+        coeff3.current
       );
     }
-    geo.attributes.position.needsUpdate = true;
-    geo.computeVertexNormals();
-    return geo;
-  }, [patchResol, offsetX, offsetY]);
+    groundGeo.attributes.position.needsUpdate = true;
+    groundGeo.computeVertexNormals();
+  });
+
+  function height(x, y, c1 = 1.0, c2 = 2.0, c3 = 8.0) {
+    return (
+      noise.noise2D(x, y) / 60 +
+      noise.noise2D(x / 10, y / 10) * c1 +
+      noise.noise2D(x / 20, y / 20) * c2 +
+      noise.noise2D(x / 40, y / 40) * c3
+    );
+  }
 
   return (
     <mesh geometry={groundGeo} receiveShadow position={[offsetX, 0, offsetY]}>
@@ -100,6 +122,7 @@ function Ground({ patchResol = 256, offsetX = 0, offsetY = 0 }) {
           fogFar: { value: 20 },
         }}
         fog={true}
+        wireframe={false}
       />
     </mesh>
   );
@@ -115,8 +138,6 @@ function Scene() {
         <Ground offsetX={0.5 * width} offsetY={-0.5 * width} />
         <Ground offsetX={-0.5 * width} offsetY={0.5 * width} />
         <Ground offsetX={0.5 * width} offsetY={0.5 * width} />
-
-        {/* <StandardEffects bloom={false} /> */}
       </Suspense>
       <OrbitControls />
     </>
